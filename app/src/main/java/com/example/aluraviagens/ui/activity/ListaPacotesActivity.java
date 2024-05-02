447ea70ce657746ebd87c50286beb1c11f98961e
Gabriel Santos <maginovam@gmail.com>
2024-05-02 16:12:33 -0300

    refatorando codigo

--- app/src/main/java/com/example/aluraviagens/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/com/example/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -1,12 +1,20 @@
-package com.example.aluraviagens;
+package com.example.aluraviagens.ui.activity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.aluraviagens.R;
+import com.example.aluraviagens.dao.PacoteDAO;
+import com.example.aluraviagens.model.Pacote;
+import com.example.aluraviagens.ui.adapter.ListaPacotesAdapter;
+
+import java.util.List;
 
 public class ListaPacotesActivity extends AppCompatActivity {
 
@@ -15,6 +23,11 @@ public class ListaPacotesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_lista_pacotes);
+        setTitle("Pacotes");
+        ListView listaDePacotes = findViewById(R.id.lista_pacotes_listview);
+        List<Pacote> pacotes = new PacoteDAO().lista();
+        listaDePacotes.setAdapter(new ListaPacotesAdapter(pacotes,this));
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
